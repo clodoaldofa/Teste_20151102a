5321a76d4434aff9d18faeefbb31f6d99c54c104
Clodoaldo Francisco de Assis <clodoaldofa@hotmail.com>
2015-11-02 15:30:18 -0200

    BA201511021519

---
+++ Teste_20151102a/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_20151102a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Minha terra tem palmeiras");
+            Console.WriteLine(", onde canta o sabiá.");
+            Console.Write("As aves que aqui gorjeiam");
+            Console.WriteLine(", não gorjeiam como lá.");
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
